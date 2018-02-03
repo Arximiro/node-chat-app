ce06b1ee2d36463e65ca05514a8d04bf408f37f0
David Ridgley <Arximiro@outlook.com>
2018-02-03 12:08:48 -0500

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -25,12 +25,13 @@ socket.on('newLocationMessage', (msg) => {
 
 $('#message-form').on('submit', (e) => {
     e.preventDefault();
+    const messageTextbox = $('[name=message]')
 
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, () => {
-    
+        messageTextbox.val('');
     });
 });
 
@@ -40,12 +41,16 @@ locationButton.on('click', () => {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, () => {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location.');
     });
 });
