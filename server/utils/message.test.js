37f9b652db24547ce3387f0dfd89a9d66e0c2e77
David Ridgley <Arximiro@outlook.com>
2018-02-02 23:44:47 -0500

    create generatemessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,16 @@
+const expect = require('expect');
+
+const {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        const res = generateMessage('David', 'Hello There');
+
+
+        expect(res).toMatchObject({
+           from: 'David',
+           text: 'Hello There'
+        });
+        expect(typeof res.createdAt).toBe('number');
+    });
+});
