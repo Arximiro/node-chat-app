78fb0067cf47845b30c72b6a0a249a319bf3c12d
David Ridgley <Arximiro@outlook.com>
2018-02-03 10:45:31 -0500

    add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,28 @@
 const socket = io();
 
 socket.on('connect', () => {
-    console.log('Connected to server');
+    console.log('Connected to server');   
 });
-
 
 socket.on('newMessage', (msg) => {
     console.log(msg);
+    const li = $('<li></li>');
+    li.text(`${msg.from}: ${msg.text}`);
+
+    $('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
+});
+
+$('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, () => {
+    
+    });
 });
