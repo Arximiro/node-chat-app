9144fb4ad0feb3c3fe687d3744dca9741f603f2d
David Ridgley <Arximiro@outlook.com>
2018-02-03 20:11:42 -0500

    add autoscroll for when appropriate

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,28 @@
 const socket = io();
 
+const scrollToBottom = () => {
+    // Selectors
+    const messages = $('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', () => {
     console.log('Connected to server');   
 });
 
+// Listens for newMessage event from server.
+// Takes the time, formats it to readable, grabs the template's html value from index.html, then renders it with Mustache passing in 3 properties.
 socket.on('newMessage', (msg) => {
     const formattedTime = moment(msg.createdAt).format('h:mm a');
     const template = $('#message-template').html();
@@ -14,8 +33,11 @@ socket.on('newMessage', (msg) => {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
+// Listens for newLocationMessage event from server.
+// Takes the time, formats it to readable, grabs the template's html value from index.html, then renders it with Mustache passing in 3 properties.
 socket.on('newLocationMessage', (msg) => {
     const formattedTime = moment(msg.createdAt).format('h:mm a');
     const template = $('#location-message-template').html();
@@ -25,9 +47,15 @@ socket.on('newLocationMessage', (msg) => {
         createdAt: formattedTime
     });
 
-    $('#messages').append(html);    
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
+// Targets element with id message-form in index.html, and creats an onSubmit handler for the form.
+// in the callback the event is passed in, then the default is prevented to stop the normal form behavior.
+// since the element with id message is selected multiple times it is stored in a variable.
+// When a message is submitted, the createMessage event is sent to the server including the from and text values
+// Lastly the messageTextbox value is set back to empty.
 $('#message-form').on('submit', (e) => {
     e.preventDefault();
     const messageTextbox = $('#message')
@@ -40,6 +68,7 @@ $('#message-form').on('submit', (e) => {
     });
 });
 
+// navigator.geolocation is built in and finds a users geolocation.
 const locationButton = $('#send-location');
 locationButton.on('click', () => {
     if (!navigator.geolocation) {
